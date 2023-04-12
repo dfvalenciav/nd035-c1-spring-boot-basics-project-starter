f285193334b660c31b5001c0f972e9da73f8fd24
Daniel Valencia <dfvalenciav@gmail.com>
2023-04-11 20:38:34 -0500

    note service

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+    private NoteMapper noteMapper;
+    private UserService userService ;
+
+    public NoteService(NoteMapper noteMapper, UserService userService) {
+        this.noteMapper = noteMapper;
+        this.userService = userService;
+    }
+
+    public int addNote (Note note, String username) {
+        String noteTitle = note.getNoteTitle();
+        String noteDescription = note.getNoteDescription();
+        Integer noteId = note.getNoteId();
+        User user = userService.getUser(username);
+        return noteMapper.insertNote(new Note(noteId, noteTitle, noteDescription, user.getUserId()));
+    }
+
+    public void updateNote (Note note, String username){
+        String noteTitle = note.getNoteTitle();
+        String noteDescription = note.getNoteDescription();
+        Integer noteId = note.getNoteId();
+        User user = userService.getUser(username);
+         this.noteMapper.updateNote(noteId, noteTitle, noteDescription, user.getUserId());
+    }
+
+    public void deleteNote (Integer noteId) { noteMapper.deleteNote(noteId);}
+
+    public void showAllNotes (Integer userId) {noteMapper.retrieveAllNotes(userId);}
+
+}
